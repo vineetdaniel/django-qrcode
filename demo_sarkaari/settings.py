e2c78cfa1a1ced0daee361282e085cbcfce1c0d4
vineetdaniel@gmail.com <vineetdaniel@gmail.com>
2018-11-01 13:51:51 +0530

    adding postgres support

--- demo_sarkaari/settings.py
+++ demo_sarkaari/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'demo_sarkaari.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbname',
+        'USER': 'postgres',
+        'PASSWORD': '513358',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
-MEDIA_ROOT= '/mnt/c/Users/2200002708/Development/python/django/demo_sarkaari/media/'
+MEDIA_ROOT= '/media/'
 MEDIA_URL = '/media/'
