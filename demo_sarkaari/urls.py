29286215ba7e6fc4b8df5db3d26eb69ed2bf43ac
vineetdaniel@gmail.com <vineetdaniel@gmail.com>
2018-11-02 07:49:08 +0530

    adsf

--- demo_sarkaari/urls.py
+++ demo_sarkaari/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^qrcode/', include('qrcodeapp.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
