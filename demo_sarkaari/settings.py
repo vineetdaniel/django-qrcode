fe0a732b00a9a5f154b4e135ee3536f25f05386f
vineetdaniel@gmail.com <vineetdaniel@gmail.com>
2018-11-02 07:57:59 +0530

    adsf

--- demo_sarkaari/settings.py
+++ demo_sarkaari/settings.py
@@ -136,14 +136,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# STATIC_ROOT = BASE_DIR + '/static'
-# STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static'
+STATIC_URL = '/static/'
 MEDIA_ROOT= BASE_DIR + '/media/'
 MEDIA_URL = '/media/'
 # django_heroku.settings(locals())
-# # STATICFILES_DIRS = (
+STATICFILES_DIRS = (
 
-#     os.path.join(BASE_DIR,STATIC_ROOT ),
-#     os.path.join(BASE_DIR, MEDIA_ROOT),
+    os.path.join(BASE_DIR,STATIC_ROOT ),
+    os.path.join(BASE_DIR, MEDIA_ROOT),
 
-#                     )
+                    )
